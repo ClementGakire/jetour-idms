1427e40b8a3b646c7669fcc28c39cdd16adef401
Clement Gakire <clementgakire@gmail.com>
2025-09-08 13:30:15 +0200

    contracts

--- vavarw/resources/views/contracts/index.blade.php
+++ vavarw/resources/views/contracts/index.blade.php
@@ -11,7 +11,7 @@
             <h3 class="text-muted text-left mb-3">Contracts</h3>
             <a href="/contracts/create" class="btn btn-primary mb-3">New Contract</a>
 
-            <table class="table table-striped">
+            <table id="contracts-table" class="display table table-striped" style="width:100%">
               <thead>
                 <tr>
                   <th>ID</th>
@@ -52,4 +52,45 @@
     </div>
   </div>
 </section>
+    <!-- DataTables scripts (copied from other index pages) -->
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script>
+
+    <script>
+      $(document).ready(function(){
+        var table = $('#contracts-table').DataTable({
+          dom: "<'row'<'col-sm-3'l><'col-sm-6 text-center'B><'col-sm-3'f>>" +
+               "<'row'<'col-sm-12'tr>>" +
+               "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+          buttons: [
+            { extend: 'excelHtml5', title: 'Contracts', footer: true, header: true },
+            { extend: 'pdfHtml5', title: 'Contracts', footer: true, header: true },
+            { extend: 'csvHtml5', title: 'Contracts', footer: true, header: true },
+            { extend: 'print', footer: true, header: true }
+          ],
+          lengthMenu: [[10,25,50,100,-1],[10,25,50,100,'All']]
+        });
+
+        // Add per-column search inputs
+        $('#contracts-table thead tr').clone(true).appendTo('#contracts-table thead');
+        $('#contracts-table thead tr:eq(1) th').each(function (i) {
+          var title = $(this).text();
+          $(this).html('<input type="text" style="width:120px;" placeholder="'+title+'" />');
+          $('input', this).on('keyup change', function () {
+            if (table.column(i).search() !== this.value) {
+              table.column(i).search(this.value).draw();
+            }
+          });
+        });
+      });
+    </script>
+
 @endsection
